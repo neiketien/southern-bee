b60aa1b0bbb7f2d17afa68b3fa8ad9ff53c4f289
neiketien <mae.neiketien@gmail.com>
2025-12-03 15:40:54 +0100

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -75,7 +75,7 @@ function googleTranslateElementInit() {
     var gtScript = document.createElement('script');
     gtScript.type = 'text/javascript';
     gtScript.async = true;
-    gtScript.src = '//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit';
+    gtScript.src = 'translate.google.com/translate_a/element.js?cb=googleTranslateElementInit';
     document.getElementsByTagName('head')[0].appendChild(gtScript);
 })();
 
